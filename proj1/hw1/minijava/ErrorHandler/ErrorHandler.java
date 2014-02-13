4e313970fdb13ae1a510423fba0a0ca8efae623e
Michael Millian <xenakaaii@gmail.com>
2014-02-13 14:08:31 -0500

    updated ErrorHandler code from project 1 to fancy regex version -MCM

--- proj1/hw1/minijava/ErrorHandler/ErrorHandler.java
+++ proj1/hw1/minijava/ErrorHandler/ErrorHandler.java
@@ -1,62 +1,34 @@
 package minijava.ErrorHandler;
-//import minijava.lexer.*;
+import minijava.lexer.*;
 import java.io.StringReader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PushbackReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
-//import minijava.node.*;
+import minijava.node.*;
 
 public class ErrorHandler {
 
     String filename;
 
     public ErrorHandler (String filename) {
-	this.filename=filename;
+		this.filename=filename;
     }
 
-    /*=====================================
-      Prints more informative error message
-      ====================================*/
     public String getLongMessage(String s) {
-
-	System.out.println("\nError during parsing: " +s);
-	int end = s.indexOf("]");
-	String[] markers = s.substring(1,end).split(",");
-	int line = Integer.valueOf(markers[0]);
-	int column = Integer.valueOf(markers[1]);
-	System.out.println("Error was found on Line "+line+", Column "+column);
-       
-	try {
-	    System.out.println(filename);
-	    BufferedReader file = new BufferedReader(new FileReader(filename));
-	    int i = 0;
-	    String errorLine=null;
-	    while (i<line) {
-		errorLine=file.readLine();
-		i++;
-	    }
-	    String temp = errorLine;
-	    while (temp.startsWith("\t")) {
-		    column=column+5;
-		    temp=temp.substring(1,errorLine.length());
-	    }
-	    System.out.println(errorLine);
-	    String mark=new String(" ");
-	    i=0;
-	    while (i<column) {
-		mark = mark.concat(" ");
-		i++;
-	    }	    
-	    mark = mark.concat("^");
-	    System.out.println(mark);
-	}
-	catch (IOException e) {
-	    System.out.println(e);
-	}
-		
-	return "BREAK";
+		String[] locus = s.replaceAll("(?i)(\\[)(.+?)(\\])(.+)", "$2").split(",");//regex to extract the numbers inside the brackets
+		System.out.println("The error was detected at line "+locus[0]+", column "+locus[1]+".\nHere is line "+locus[0]+". The caret mark (^) indicates where the error was detected.");
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(filename));// the FileReader constructor can take a String filename
+			String line="";
+			for (int i=0 ; i<Integer.parseInt(locus[0]); i++){//get line locus[0]
+				line=reader.readLine();
+			}
+			System.out.println(line.replaceAll("(\\t)", " "));//replace tabs
+			System.out.println(String.format("%"+locus[1]+"s","^"));//draw caret
+		} catch (Exception e){
+			System.out.println("Error reading File "+filename+".");
+		}
+		throw new UnsupportedOperationException();
     }
 }
